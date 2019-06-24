09a605dba82eb53aafaa6653c2139730a02ca14c
Will Fritts <waf515@gmail.com>
2019-06-24 10:18:52 -0700

    initial setup

---
+++ src/main/java/com/team/long/java401d/midterm/troublemaker/TroublemakerApplication.java
@@ -0,0 +1,13 @@
+package com.team.long.java401d.midterm.troublemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroublemakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroublemakerApplication.class, args);
+	}
+
+}
