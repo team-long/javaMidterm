1954fec69c809821bb5ccc06c036fa1d774f9b81
Will Fritts <waf515@gmail.com>
2019-06-25 13:29:16 -0700

    added all tickets route, and fixed redirect to /main instead of /profile

--- src/main/java/com/teamLong/java401d/midterm/troublemaker/controller/TicketController.java
+++ src/main/java/com/teamLong/java401d/midterm/troublemaker/controller/TicketController.java
@@ -6,6 +6,7 @@ import com.teamLong.java401d.midterm.troublemaker.repository.TicketRepository;
 import com.teamLong.java401d.midterm.troublemaker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
@@ -31,8 +32,17 @@ public class TicketController {
         UserAccount user = userRepository.findByUsername(p.getName());
         Ticket ticket = new Ticket(severity, user, summary);
         ticketRepository.save(ticket);
-        return new RedirectView("/profile");
+        return new RedirectView("/main");
     }
 
     //all tickets user route
+    @GetMapping("/tickets/all")
+    public String getAllTickets(Principal principal, Model model){
+        UserAccount loggedInUser = userRepository.findByUsername(principal.getName());
+        model.addAttribute("loggedInUser", loggedInUser);
+        Iterable<Ticket> tickets = ticketRepository.findAll();
+        model.addAttribute("tickets", tickets);
+        return "ticket-detail";
+    }
+
 }
