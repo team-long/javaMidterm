1dd4c4954a9ded186166ea79e917fd6c96e2f0e1
Will Fritts <waf515@gmail.com>
2019-06-25 10:12:38 -0700

    ticket model, ticket controller, header/footer fragment done

---
+++ src/main/java/com/teamLong/java401d/midterm/troublemaker/Tickets/Ticket.java
@@ -0,0 +1,51 @@
+package com.teamLong.java401d.midterm.troublemaker.Tickets;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    LocalDateTime createdAt;
+    short severity;
+    String creator;
+    String summary;
+
+    public Ticket(){}
+
+    public Ticket(short severity, String creator, String summary){
+        this.createdAt = LocalDateTime.now();
+        this.severity = severity;
+        this.creator = creator;
+        this.summary = summary;
+    }
+
+    //database relation
+//    @ManyToOne
+//    UserAccount creator;
+
+    //getters & setters
+
+    public long getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public short getSeverity() {
+        return severity;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+}
---
+++ src/main/java/com/teamLong/java401d/midterm/troublemaker/Tickets/TicketController.java
@@ -0,0 +1,33 @@
+package com.teamLong.java401d.midterm.troublemaker.Tickets;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
+
+@Controller
+public class TicketController {
+
+//    @Autowired
+//    UserRepository userRepository;
+//
+//    @Autowired
+//    TicketRepository ticketRepository;
+
+    @GetMapping("/ticket")
+    public String createTicketPage(){
+        return "ticket";
+    }
+
+//    @PostMapping("/create/ticket")
+//    public RedirectView makeATicket(String summary, short severity, Principal p){
+//        UserAccount user = userRepository.findByUsername(p.getName());
+//        Ticket ticket = new Ticket(summary, severity, user);
+//        ticketRepository.save(ticket);
+//        return new RedirectView("/profile");
+//    }
+}
