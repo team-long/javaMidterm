20885acdfe188cc9eb1507af307b1de1356c108e
Will Fritts <waf515@gmail.com>
2019-06-26 16:20:07 -0700

    added repository for archive

--- src/main/java/com/teamLong/java401d/midterm/troublemaker/repository/ArchiveRepository.java
+++ src/main/java/com/teamLong/java401d/midterm/troublemaker/repository/ArchiveRepository.java
@@ -1,4 +1,8 @@
 package com.teamLong.java401d.midterm.troublemaker.repository;
 
-public interface ArchiveRepository {
+import com.teamLong.java401d.midterm.troublemaker.model.Archive;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArchiveRepository extends CrudRepository<Archive, Long> {
+
 }
