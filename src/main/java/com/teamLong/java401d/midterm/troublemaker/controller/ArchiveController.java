73ea9583315991ff3b0066cc15e61eba49df766d
Will Fritts <waf515@gmail.com>
2019-06-26 16:48:43 -0700

    working on delete/archive post

--- src/main/java/com/teamLong/java401d/midterm/troublemaker/controller/ArchiveController.java
+++ src/main/java/com/teamLong/java401d/midterm/troublemaker/controller/ArchiveController.java
@@ -11,7 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.view.RedirectView;
-
+import com.teamLong.java401d.midterm.troublemaker.model.Severity;
 import java.security.Principal;
 
 @Controller
@@ -29,7 +29,14 @@ public class ArchiveController {
     @DeleteMapping("delete/ticket/{id}")
     public RedirectView deleteTicket(@PathVariable long id, Principal principal, Model model){
         Ticket ticket = ticketRepository.findById(id);
-        Archive archiveTicket = new Archive(ticket.getTitle(), ticket.getTicketLvl(), ticket.getCreator(), ticket.getSummary());
+//        Archive archiveTicket = new Archive(ticket.getTitle(), ticket.getTicketLvl(), ticket.getCreator(), ticket.getSummary());
+        Archive archiveTicket = new Archive();
+        archiveTicket.setTitle(ticket.getTitle());
+//        archiveTicket.setTicketLvl(Severity.valueOf(ticket.getTicketLvl()));
+//        archiveTicket.setTicketLvl(ticket.getTicketLvl());
+//        archiveTicket.setTicketLvl(ticket.ticketLvl);
+        archiveTicket.setCreator(ticket.getCreator());
+        archiveTicket.setSummary(ticket.getSummary());
         if(ticket.getCreator().getUsername().equals(principal.getName())){
             ticketRepository.deleteById(id);
         } else {
