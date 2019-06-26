6909d308d896f5625d6045233e9087235ea8a186
Will Fritts <waf515@gmail.com>
2019-06-26 10:04:52 -0700

    added ticket update, ticket delete, ticket edit routes

--- src/main/java/com/teamLong/java401d/midterm/troublemaker/controller/TicketController.java
+++ src/main/java/com/teamLong/java401d/midterm/troublemaker/controller/TicketController.java
@@ -1,14 +1,15 @@
 package com.teamLong.java401d.midterm.troublemaker.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import com.teamLong.java401d.midterm.troublemaker.model.Ticket;
 import com.teamLong.java401d.midterm.troublemaker.model.UserAccount;
 import com.teamLong.java401d.midterm.troublemaker.repository.TicketRepository;
 import com.teamLong.java401d.midterm.troublemaker.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.security.Principal;
@@ -45,4 +46,37 @@ public class TicketController {
         return "allTickets";
     }
 
+    @GetMapping("/edit/{id}")
+    public String editTicket(@PathVariable long id, Principal principal, Model model){
+        Ticket ticket = ticketRepository.findById(id);
+        model.addAttribute("ticket", ticket);
+        UserAccount loggedInUser = userRepository.findByUsername(principal.getName());
+        model.addAttribute("loggedInUser", loggedInUser);
+        return "edit";
+    }
+
+//    @PutMapping("/tickets/edit/{id}")
+//    public RedirectView updateTicket(@ModelAttribute("ticket") Ticket ticket){
+//        ticketRepository.update(ticket);
+//        return new RedirectView("allTickets");
+//    }
+
+    @DeleteMapping("delete/ticket/{id}")
+    public RedirectView deleteTicket(@PathVariable long id, Principal principal, Model model){
+        Ticket ticket = ticketRepository.findById(id);
+        if(ticket.getCreator().equals(principal.getName())){
+            ticketRepository.deleteById(id);
+        } else {
+            throw new TicketDoesNotBelongToYou("There is only one thing we say to death. Not today.\n You do not own this ticket");
+        }
+        return new RedirectView("allTickets");
+    }
+
+}
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN)
+class TicketDoesNotBelongToYou extends RuntimeException {
+    public TicketDoesNotBelongToYou(String string){
+        super(string);
+    }
 }
